15a63362f0e7e57cd8355dd325d6809f3520e929
Rafal Dittwald <rafal.dittwald@gmail.com>
2025-10-16 16:27:28 -0400

    Add missing dependencies to project.clj

--- projects/basics/project.clj
+++ projects/basics/project.clj
@@ -1,4 +1,7 @@
 (defproject big-demo "0.1.0-SNAPSHOT"
   :description "Basics"
-  :dependencies [[org.clojure/data.codec "0.1.1"]]
+  :dependencies [[org.clojure/data.codec "0.1.1"]
+                 [com.clojure-goes-fast/clj-java-decompiler "0.3.6"]
+                 [com.github.seancorfield/next.jdbc "1.3.994"]
+                 [com.h2database/h2 "2.2.224"]]
   :resource-paths ["src" "resources"])
