0712cf890c822d1024668279156693edf9c62e1c
Juan Monetta <jpmonettas@gmail.com>
2025-12-05 10:39:17 -0300

    Delete projects/cljs_compiler/.repl-0.0.340664477

--- projects/cljs_compiler/.repl-0.0.340664477/EF9FE3E.js
+++
@@ -1,9 +0,0 @@
-goog.provide('clojure.browser.repl.client');
-goog.require('cljs.core');
-goog.require('goog.events');
-goog.require('clojure.browser.repl');
-clojure.browser.repl.client.start = (function clojure$browser$repl$client$start(url){
-return goog.events.listen(window,"load",(function (){
-return clojure.browser.repl.start_evaluator.call(null,url);
-}));
-});
